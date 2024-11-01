7d2c9a3bfb265d36bfda7efbd0f8f05b4208f581
Brian Sanchez <brian@rokk3rlabs.com>
2024-11-01 14:04:13 -0400

    ✨ feat(index.js): add support for player actions in the game session
    🚀 feat(player): add generated TypeScript file for player actions with Action, ActionType, Position, Velocity, and PlayerAction structures
    
    🔧 fix(index.ts): remove unnecessary console.log statements
    🚀 feat(index.ts): add support for encoding and decoding player actions

--- examples/node/dist/examples/node/src/index.js
+++ examples/node/dist/examples/node/src/index.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const session_1 = require("../../../generated/typescript/game/session");
 const player_1 = require("../../../generated/typescript/player/player");
+const action_1 = require("../../../generated/typescript/player/action");
 const playerType = player_1.Player_PlayerType.HUMAN;
 const playerStatus = player_1.Player_PlayerStatus.ACTIVE;
 const samplePlayer = {
@@ -14,7 +15,6 @@ const samplePlayer = {
     status: playerStatus,
     role: 'admin',
 };
-console.log(samplePlayer);
 const difficultyAttribute = {
     key: 'difficulty',
     intValue: 1,
@@ -33,7 +33,6 @@ const session = {
         max_score: maxScoreAttribute,
     },
 };
-console.log(session);
 const encodedPlayer = player_1.Player.encode(samplePlayer).finish();
 console.log(encodedPlayer);
 const decodedPlayer = player_1.Player.decode(encodedPlayer);
@@ -42,3 +41,22 @@ const encodedSession = session_1.Session.encode(session).finish();
 console.log(encodedSession);
 const decodedSession = session_1.Session.decode(encodedSession);
 console.log(decodedSession);
+const playerAction = {
+    key: 'jump',
+    stringValue: 'high',
+};
+const action = {
+    playerId: 'player1',
+    sessionId: 'session1',
+    gameId: 'game1',
+    actionType: action_1.Action_ActionType.CUSTOM,
+    action: {
+        playerAction: playerAction,
+    },
+    // send NTP timestamp provided by the server
+    timestamp: Date.now(),
+};
+const encodedAction = action_1.Action.encode(action).finish();
+console.log(encodedAction);
+const decodedAction = action_1.Action.decode(encodedAction);
+console.log(decodedAction);
--- examples/node/src/index.ts
+++ examples/node/src/index.ts
@@ -1,5 +1,6 @@
 import { Session, Session_GameAttribute, Session_GameState } from '../../../generated/typescript/game/session';
 import { Player, Player_PlayerStatus, Player_PlayerType } from '../../../generated/typescript/player/player';
+import { Action, Action_ActionType, Action_PlayerAction } from '../../../generated/typescript/player/action';
 
 const playerType = Player_PlayerType.HUMAN;
 const playerStatus = Player_PlayerStatus.ACTIVE;
@@ -14,8 +15,6 @@ const samplePlayer: Player = {
   status: playerStatus,
   role: 'admin',
 };
-
-console.log(samplePlayer);
 
 const difficultyAttribute: Session_GameAttribute = {
   key: 'difficulty',
@@ -38,8 +37,6 @@ const session: Session = {
   },
 }
 
-console.log(session);
-
 const encodedPlayer = Player.encode(samplePlayer).finish();
 console.log(encodedPlayer);
 
@@ -51,3 +48,26 @@ console.log(encodedSession);
 
 const decodedSession = Session.decode(encodedSession);
 console.log(decodedSession);
+
+const playerAction: Action_PlayerAction = {
+  key: 'jump',
+  stringValue: 'high',
+};
+
+const action: Action = {
+  playerId: 'player1',
+  sessionId: 'session1',
+  gameId: 'game1',
+  actionType: Action_ActionType.CUSTOM,
+  action: {
+    playerAction: playerAction,
+  },
+  // send NTP timestamp provided by the server
+  timestamp: Date.now(),
+};
+
+const encodedAction = Action.encode(action).finish();
+console.log(encodedAction);
+
+const decodedAction = Action.decode(encodedAction);
+console.log(decodedAction);
