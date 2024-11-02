56e7159f6c919d02b25b178f375f5ec9a7f32343
Brian Sanchez <brian@rokk3rlabs.com>
2024-11-02 10:03:35 -0400

    🚀 test(ballcar.test.js): update test description for encoding and decoding the ball entity 🚀 test(ballcar.test.js): add test for encoding and decoding the ballcar state

--- tests/node/dist/tests/node/tests/ballcar.test.js
+++ tests/node/dist/tests/node/tests/ballcar.test.js
@@ -2,7 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const globals_1 = require("@jest/globals");
 (0, globals_1.describe)('ballcar module', () => {
-    (0, globals_1.test)('should encode and decode the ballcar object', () => {
+    // TODO: Implement entity protobufs
+    (0, globals_1.test)('should encode and decode the ball entity', () => {
+        (0, globals_1.expect)(true).toBe(true);
+    });
+    (0, globals_1.test)('should encode and decode the ballcar state', () => {
         (0, globals_1.expect)(true).toBe(true);
     });
 });
