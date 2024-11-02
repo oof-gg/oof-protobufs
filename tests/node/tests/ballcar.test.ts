4d8e1ef4b6a5bad8f7d3cb8abcd7d00549ff1188
Brian Sanchez <brian@rokk3rlabs.com>
2024-11-02 10:02:53 -0400

    🚀 test(ballcar.test.ts): update test descriptions for ball entity and ballcar state 🔧 chore(tsconfig.json): add path mapping for @bufbuild/* to node_modules/@bufbuild/*

--- tests/node/tests/ballcar.test.ts
+++ tests/node/tests/ballcar.test.ts
@@ -1,7 +1,13 @@
 import { describe, expect, test } from "@jest/globals";
 
 describe('ballcar module', () => {
-  test('should encode and decode the ballcar object', () => {
+  // TODO: Implement entity protobufs
+  test('should encode and decode the ball entity', () => {
     expect(true).toBe(true);
   });
+
+  test('should encode and decode the ballcar state', () => {
+    expect(true).toBe(true);
+  });
+  
 });
