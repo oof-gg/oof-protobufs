bf0304efd0f5be1e474ed925ed94df332df3d046
Brian Sanchez <brian@rokk3rlabs.com>
2024-12-20 14:29:25 -0500

    🔧 fix(go): update import paths to use versioned package structure in generated.go

--- generated/go/v1/generated.go
+++ generated/go/v1/generated.go
@@ -2,9 +2,9 @@
 package generated
 
 import (
-  - "github.com/oof-gg/oof-protobufs/generated/go/api/auth"
-	_ "github.com/oof-gg/oof-protobufs/generated/go/api/game"
-	_ "github.com/oof-gg/oof-protobufs/generated/go/api/global"
-	_ "github.com/oof-gg/oof-protobufs/generated/go/api/player"
-	_ "github.com/oof-gg/oof-protobufs/generated/go/std"
+  - "github.com/oof-gg/oof-protobufs/generated/go/v1/api/auth"
+	- "github.com/oof-gg/oof-protobufs/generated/go/v1/api/game"
+	_ "github.com/oof-gg/oof-protobufs/generated/go/v1/api/global"
+	_ "github.com/oof-gg/oof-protobufs/generated/go/v1/api/player"
+	_ "github.com/oof-gg/oof-protobufs/generated/go/v1/std"
 )
